6de3b9cadf47cbc8aaa7c24171744abde80332b1
Sayem <sayem465508@gmail.com>
2023-11-18 09:00:31 +0600

    more about closure

---
+++ closure.js
@@ -0,0 +1,17 @@
+const outerFunction = () => {
+
+    let counter = 0;
+
+    return () => {
+        counter++;
+        console.log('Count: ',counter)
+    }
+}
+
+const x1 = outerFunction();
+const x2 = outerFunction();
+x1()
+x1()
+x2()
+x1();
+x2();
